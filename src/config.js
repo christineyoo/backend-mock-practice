c8004b65173dc6a9783b99ebfa0887ec1d54b154
Christine Yoo <christinecho.s@gmail.com>
2021-05-26 16:52:07 -0700

    added environment variables in config file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://chris@localhost/collab',
+    CLIENT_ORIGIN: {
+      origin: 'http://localhost:3000',
+      methods: 'GET'
+    }
   }
